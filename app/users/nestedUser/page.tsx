749ed3c655d9dc225014497f713aa511e7b1f3fc
hasibul islam <hasibulislamha01@gmail.com>
2025-02-03 23:40:44 +0600

    initialized next-app

---
+++ app/users/nestedUser/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-rose-400 text-lg'>
+      Nested user here
+    </div>
+  )
+}
+
+export default page
---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <section>
+        <h1 className='text-green-700 font-semibold text-lg'>User Page</h1>
+    </section>
+  )
+}
+
+export default UsersPage
