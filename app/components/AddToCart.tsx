375ebf4494bd2b5efa36d95fe023f0e0c0864d5c
hasibul islam <hasibulislamha01@gmail.com>
2025-02-04 22:53:25 +0600

    learned about data caching

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 
 const AddToCart = () => {
     return (
-        <button onClick={() => console.log('clicked')} className='px-5 py-1 bg-gray-700 text-white rounded-md active:scale-95'>Action</button>
+        <button
+            onClick={() => console.log('clicked')}
+            className='px-5 py-1 bg-gray-700 text-white rounded-md active:scale-95'>
+            Action
+        </button>
     )
 }
 
