21dc53cb09ec18939b193b93e732c6ece1df4a3c
hasibul islam <hasibulislamha01@gmail.com>
2025-02-19 23:40:58 +0600

    added shadcn ui and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
-import Link from "next/link";
+import { Navbar } from "@/components/SharedComponents/Navbar/Navbar"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      <section className="bg-slate-700 text-white font-semibold py-3 px-5 rounded-lg flex items-center gap-12">
-        <Link
-          href='/users'
-          className="hover:underline py-1 transition-all duration-300"
-        >
-          Users
-        </Link>
-        {/* <Link
-          href='/users/nestedUser'
-          className="hover:underline py-1 transition-all duration-300 "
-        >
-          Nested Users
-        </Link> */}
-        <Link
-          href='/products'
-          className="hover:underline py-1 transition-all duration-300 "
-        >
-          Products
-        </Link>
-      </section>
+      <section className="w-full"><Navbar/></section>
       <h1>Hello Next js </h1>
+      <Button>Hellow ShadCn Button</Button>
     </main>
   );
 }
